8976a9f56e931f17b3803a7ad4babfd19ac52037
Lakshman Karunasena <lakkaru@gmail.com>
2023-01-01 21:46:21 +0530

    feat - suffeled images attached to the cards

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //enabling strict mode
-"use strict"
+"use strict";
 //creating a array for images
 const imageArray = [
   { name: "bird", image: "images/bird.png" },
@@ -15,9 +15,14 @@ const imageArray = [
   { name: "mouse", image: "images/mouse.png" },
   { name: "owl", image: "images/owl.png" },
 ];
+const choosenCards = [];
 
 //assigning images to grid cards
 const grid = document.querySelector("#grid");
+
+//suffling the images
+imageArray.sort(() => 0.5 - Math.random());
+// console.log(imageArray);
 
 function createGrid() {
   imageArray.forEach((image, index) => {
@@ -25,8 +30,16 @@ function createGrid() {
     card.setAttribute("src", "images/question.jpeg");
     card.setAttribute("id", index); //what is data-id?
     card.setAttribute("class", "image");
+    card.addEventListener("click", flipCard);
     grid.appendChild(card);
   });
 }
 createGrid();
 
+//flipping card to image
+function flipCard() {
+  const cardId = this.getAttribute("id");
+  choosenCards.push(cardId);
+  //   console.log(choosenCards);
+  this.setAttribute("src", imageArray[cardId].image);
+}
